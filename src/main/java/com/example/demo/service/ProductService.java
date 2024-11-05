59e3cadd3385ce37b3cc77030488c28e8dd0d723
IVSON SANTOS <ivson.santos@talan.com>
2024-11-05 13:34:14 +0100

    Created

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -14,10 +15,34 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    //@PostConstruct
     public List<Product> findAll() {
         return productRepository.findAll();
     }
+
+    public List<Product> getOrdered() {
+        List<Product> products = productRepository.findAll();
+        // TODO implement the logic here
+        return null;
+    }
+
+    public Double getAverage() {
+        List<Product> products = productRepository.findAll();
+        // TODO implement the logic here
+        return null;
+    }
+
+    public Integer getTotal() {
+        List<Product> products = productRepository.findAll();
+        // TODO implement the logic here
+        return null;
+    }
+
+    public Product getById(Integer id) {
+        Optional<Product> product = productRepository.findById(id);
+        // TODO implement the logic here
+        return null;
+    }
+
 
 
 }
