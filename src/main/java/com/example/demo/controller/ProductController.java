03245a5e4ab4f2d68330cb59a962ece5cacf3eb0
IVSON SANTOS <ivson.santos@talan.com>
2024-11-05 12:35:49 +0100

    Created

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/all")
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+}
